fa2472be25213f0cb9c37d9235f0b38b59852eda
shravyagugulothu-arch <shravyagugulothu@gmail.com>
2026-06-27 05:02:42 +0000

    Add DAY2 solutions

---
+++ DAY2/program8.py
@@ -0,0 +1,12 @@
+name1="ACE"
+name2="Engineering college"
+name3=name1+name2
+print(name3)
+print(name3[0:3])
+print(name3[:3])
+print(name3[0:16])
+print(name3[3:])
+print(name3[:])
+print(name3[-1])
+name4=name1[0].lower()+name1[1:]
+print(name4)
